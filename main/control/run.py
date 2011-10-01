6a8fdf55aa33e9b65cf17150cc4665347cfd82a5
G M <giomagi@gmail.com>
2011-10-01 12:17:24 +0100

    server and data acquisition independent start/stop

--- main/control/run.py
+++ main/control/run.py
@@ -1,20 +1,53 @@
 import BaseHTTPServer
+import sys
+import datetime
 from main.houses.foxtons import Foxtons
 from main.houses.persistence import Librarian
 from main.web.webserver import GetPropertiesHandler
 
-if __name__ == '__main__':
-    server_class = BaseHTTPServer.HTTPServer
-    httpd = server_class(('192.168.1.2', 1234), GetPropertiesHandler)
-
+def startDataAcquisition():
+    print 'Start Data Acquisition @ ' + str(datetime.datetime.now())
     Librarian().archiveProperties(Foxtons().properties())
+    print 'Data Acquisition Completed @ ' + str(datetime.datetime.now())
 
-    print "UP"
+def startWeb():
+    server_class = BaseHTTPServer.HTTPServer
+    webserver = server_class(('192.168.1.2', 1234), GetPropertiesHandler)
 
+    print "Web Server UP"
     try:
-        httpd.serve_forever()
+        webserver.serve_forever()
     except KeyboardInterrupt:
         pass
-    httpd.server_close()
+    
+    webserver.server_close()
+    print "Web Server DOWN"
 
-    print "DOWN"
+def stopWeb():
+    raise Exception, 'Web stop supported yet through keyboard interrupt (for the time being)'
+
+
+def fail():
+    raise Exception, 'Expected run <start|stop> <data|web>, obtained ' + str(sys.argv[1:])
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3: fail()
+
+    command = sys.argv[1]
+    what = sys.argv[2]
+
+    if what == 'data':
+        if command == 'start':
+            startDataAcquisition()
+        else:
+            raise Exception, 'Data acquisition supports only start'
+
+    elif what == 'web':
+        if command == 'start':
+            startWeb()
+        elif command == 'stop':
+            stopWeb()
+        else:
+            raise Exception, 'Web supports only start and stop commands'
+
+    else: fail()
