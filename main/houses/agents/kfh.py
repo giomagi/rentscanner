9b2be3323e44eae31cf4a24dd1ab3b46517ece5b
G M <giomagi@gmail.com>
2011-11-05 13:53:56 +0000

    removed circular reference

--- main/houses/agents/kfh.py
+++ main/houses/agents/kfh.py
@@ -1,15 +1,17 @@
 import locale
 import re
 from main.houses.agents.base_extractors import RssBasedExtractor
-from main.houses.loader import Filter
 
 class KFH(RssBasedExtractor):
     def __init__(self):
         RssBasedExtractor.__init__(self)
         self._titlePattern = re.compile(r'([^,]*,[^,]*).*\s+(\S+)\s+-\s+.(\d?,?\d+)\.\d+\s+-\s+\S+\s+(\S+)')
 
+    def interestingZones(self):
+        return "NW1", "NW3", "NW8", "SW1", "SW3", "SW5", "SW6", "SW7", "SW10", "SW11", "W1", "W2", "W8", "W11", "W14", "WC1", "WC2"
+
     def agentURIs(self):
-        return ['http://www.kfh.co.uk/search/rss.aspx?Section=Home&searchType=1&searchTerm=' + postcode + '&lat=&lng=&zoom=6&tenure=Per%20Month&order_by=price_desc&minprice=1300&maxprice=2200&minbeds=102&type=r&t=Thumbnail' for postcode in Filter.interestingZones()]
+        return ['http://www.kfh.co.uk/search/rss.aspx?Section=Home&searchType=1&searchTerm=' + postcode + '&lat=&lng=&zoom=6&tenure=Per%20Month&order_by=price_desc&minprice=1300&maxprice=2200&minbeds=102&type=r&t=Thumbnail' for postcode in self.interestingZones()]
 
     def agent(self):
         return 'KFH'
--- main/houses/loader.py
+++ main/houses/loader.py
@@ -1,14 +1,15 @@
 from main.houses.agents.foxtons import Foxtons
+from main.houses.agents.kfh import KFH
 from main.houses.agents.knight_frank import KnightFrank
 from main.houses.agents.winkworth import Winkworth
 from main.houses.persistence import Librarian
 
-class Loader():
+class Loader:
     def __init__(self):
-        self.interestingZones = Filter.interestingZones()
+        self.interestingZones = "NW1", "NW3", "NW8", "SW1", "SW3", "SW5", "SW6", "SW7", "SW10", "SW11", "W1", "W2", "W8", "W11", "W14", "WC1", "WC2"
 
     def loadAll(self):
-        for agent in [Foxtons, Winkworth, KnightFrank]:
+        for agent in [KFH, Foxtons, Winkworth, KnightFrank]:
             print 'Start ' + str(agent)
 
             newProperties = agent().properties()
@@ -22,8 +23,3 @@ class Loader():
 
     def isInteresting(self, property):
         return property.price.monthlyPrice() > 1300 and property.price.monthlyPrice() < 2200 and property.address.postcode in self.interestingZones
-
-class Filter:
-    @classmethod
-    def interestingZones(cls):
-        return "NW1", "NW3", "NW8", "SW1", "SW3", "SW5", "SW6", "SW7", "SW10", "SW11", "W1", "W2", "W8", "W11", "W14", "WC1", "WC2"
